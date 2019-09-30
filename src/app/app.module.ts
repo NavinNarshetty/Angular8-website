43367b476b3dccb6e709dd280fe32063b24570de
navinnarshetty <navinnarshetty@gmail.com>
2019-09-30 21:34:50 +0530

    singup-login form using service has been completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from "@angular/forms";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { AuthComponent } from './auth/auth.component';
+import { Routes, RouterModule } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { HttpClientModule } from "@angular/common/http";
+import { LoaderComponent } from './loader/loader.component';
+import { AuthGaurd } from './auth/auth.gaurd';
+
+
+const appRouter: Routes = [{
+  path: 'login',
+  component: AuthComponent
+}, {
+  path: 'sign-up',
+  component: AuthComponent
+}, {
+  path: 'welcome',
+  component: WelcomeComponent,
+  canActivate: [AuthGaurd]
+}]
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    AuthComponent,
+    WelcomeComponent,
+    LoaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRouter),
   ],
   providers: [],
   bootstrap: [AppComponent]
